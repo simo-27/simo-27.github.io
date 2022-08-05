40371da0dc3a2dd98ceb2212b9e9ddab7cf84bb3
Simone-D <simone.daquila27@gmail.com>
2022-08-05 16:39:29 +0200

    "Chi Sono" - aggiunta descrizione e carosello

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/* carosello di parole */
+
+var words = ['CODING', 'START-UP', 'MMA', 'VIAGGI', 'VIRTUAL REALITY', 'MEDITAZIONE'];
+var counter = 0;
+var word = document.getElementById("carousel-words");
+setInterval(update_carousel_words, 1000);
+
+function update_carousel_words(){
+word.innerHTML = words[counter];
+counter++;
+if (counter >= words.length) {
+    counter = 0;
+}
+}
